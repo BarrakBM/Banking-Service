2e005287017250d2f6069069fede76f2dce3d116
BarrakBM <barrak_mandani@live.com>
2025-04-13 23:38:41 +0300

    Adding the relationship in usersRepo

--- src/main/kotlin/com/example/Banking_Service/users/UsersRepository.kt
+++ src/main/kotlin/com/example/Banking_Service/users/UsersRepository.kt
@@ -16,7 +16,13 @@ data class UserEntity(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val username: String,
-    val passkey: String
+    val passkey: String,
+
+    //This defines a oneToMany relationship (a user can have many accounts)
+    // mapped by specify the foreign key in accounts
+    // etc... user knows about its accounts and vice versa
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL] )
+    val accounts: List<AccountEntity> = emptyList()
 ){
     constructor(): this(null, "", "")
 }
